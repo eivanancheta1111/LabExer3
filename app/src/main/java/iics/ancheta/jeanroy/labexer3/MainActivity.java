f012625c0e99335b1b1efdb6e4ea774c9782d010
eivanancheta1111 <eivan.ancheta@gmail.com>
2018-09-08 16:24:25 +0800

    Initial commit

---
+++ app/src/main/java/iics/ancheta/jeanroy/labexer3/MainActivity.java
@@ -0,0 +1,15 @@
+package iics.ancheta.jeanroy.labexer3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.relative_layout);
+        //setContentView(R.layout.linear_layout);
+        //setContentView(R.layout.activity_main);
+    }
+}
